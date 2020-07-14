0c10846fc11a46e498107b69ba42380d831cc54c
romibaby <romibaby@gmail.com>
2020-07-15 01:14:18 +1000

    first changes

---
+++ home.js
@@ -0,0 +1,60 @@
+        console.log('hello');
+      //  alert('hello this is Rachelle');
+var b = 'smothie';
+console.log(b);
+
+var someNumber = 45;
+//console.log(someNumber);
+
+//var age = prompt('Age?');
+// document.getElementById('someText').innerHTML = age;
+
+ var num1 = 10;
+
+ num1++;
+
+
+ console.log(num1);
+
+
+ num1--;
+ console.log(num1);
+
+ console.log(num1 / 6);
+
+
+ /* Fuction*/
+ function fun(){
+   console.log('thif ')
+ }
+
+ fun();
+
+
+ function greeting(yourName){
+   
+   var result = 'Hello' + ' ' + yourName;
+   console.log(result);
+ }
+
+ var name = prompt('What is your name?');
+ greeting(name);
+
+ function someNumbers(num1, num2)
+ {
+   var result = num1 + num2;
+   console.log(result);
+ }
+
+ //function someNumbers(num1: any, num2: any); void
+ someNumbers(10, 10);
+
+ // while loops
+
+ var num = 0;
+
+
+ // for loop
+ for (let num = 0; num < 100; num++) {
+ console.log(num);
+ }
